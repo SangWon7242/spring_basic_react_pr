e61735e8c0d0f42fcb71618de60ad42e9fc5c77f
SangWon7242 <gotkdjqa@gmail.com>
2024-09-11 12:57:39 +0900

    @RequestMapping(/api) 변경

---
+++ src/main/java/com/psw/sbb/basic/boundedContext/article/ArticleController.java
@@ -0,0 +1,7 @@
+package com.psw.sbb.basic.boundedContext.article;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ArticleController {
+}
--- src/main/java/com/psw/sbb/basic/boundedContext/home/HomeController.java
+++ src/main/java/com/psw/sbb/basic/boundedContext/home/HomeController.java
@@ -2,11 +2,13 @@ package com.psw.sbb.basic.boundedContext.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/api")
 public class HomeController {
-  @GetMapping("/api/home")
+  @GetMapping("/home")
   @ResponseBody
   public String showHome() {
     return "어서와";
